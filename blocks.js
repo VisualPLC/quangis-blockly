b684046ca140a38d059965da109e9ea6fee5fc37
HaiqiXu <54269764+HaiqiXu@users.noreply.github.com>
2022-02-03 12:07:39 +0100

    Update blocks.js

--- blocks.js
+++ blocks.js
@@ -1,5 +1,5 @@
-// const express = require('express');
-// const app = express();
+const express = require('express');
+const app = express();
 //
 // app.listen(3000, function() {
 //   console.log('listening on 3000')
